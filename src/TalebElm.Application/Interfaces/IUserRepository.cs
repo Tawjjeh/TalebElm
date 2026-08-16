f4ac0be1b9ccf4e5fbdb4f18d7072cecdfce17bc
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-08-17 02:47:51 +0300

    feat: add UserRepository class and IUserRepotitory Interface

---
+++ src/TalebElm.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalebElm.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<User>> GetAllAsync();
+        Task AddAsync(User entity);
+        Task UpdateAsync(User entity);
+        Task RemoveAsync(Guid id);
+    }
+}
---
+++ src/TalebElm.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalebElm.Infrastructure.Repositories
+{
+    internal class UserRepository
+    {
+    }
+}
