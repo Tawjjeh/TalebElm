d4372927b586b7a2dd3ea53d91d7182b61485328
Abdullah Hashem <abdallahhashem326@gmail.com>
2026-08-25 03:45:15 +0300

    feat: add Module entity

---
+++ src/TalebElm.Domain/Entities/Module.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalebElm.Domain.Entities
+{
+    public class Module
+    {
+        public Guid Id { get; set; }
+        public string Title{ get; set; }= string.Empty;
+        public string Summary { get; set; }=string.Empty;
+        public int Order { get; set; }
+        public Guid TrackId { get; set; }
+
+    }
+}
