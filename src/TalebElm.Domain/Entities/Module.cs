e049a4b74d862556109c03dc9598f5a16cdd72df
Abdullah Hashem <abdallahhashem326@gmail.com>
2026-08-25 04:05:48 +0300

    class Module  Inherit from Base Entity

--- src/TalebElm.Domain/Entities/Module.cs
+++ src/TalebElm.Domain/Entities/Module.cs
@@ -4,9 +4,8 @@ using System.Text;
 
 namespace TalebElm.Domain.Entities
 {
-    public class Module
+    public class Module : BaseEntity
     {
-        public Guid Id { get; set; }
         public string Title{ get; set; }= string.Empty;
         public string Summary { get; set; }=string.Empty;
         public int Order { get; set; }
