c87cc73d82c8607bea06d7ff594ebe0e26a36239
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-08-28 20:17:04 +0300

    feat:crete UserRepository and it's related interface

---
+++ src/TalebElm.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TalebElm.Domain.Entities;
+
+namespace TalebElm.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<User>> GetAllAsync();
+        Task AddAsync(User entity);
+        Task UpdateAsync(User entity);
+        Task DeleteAsync(User entity);
+    }
+}
---
+++ src/TalebElm.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalebElm.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
