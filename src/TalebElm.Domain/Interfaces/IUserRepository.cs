73458c0ea2d1844b06fd5a6a4224fe447286f00e
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-08-28 23:01:04 +0300

    refactor: return to the originl interface

--- src/TalebElm.Domain/Interfaces/IUserRepository.cs
+++ src/TalebElm.Domain/Interfaces/IUserRepository.cs
@@ -1,13 +1,7 @@
-﻿
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using TalebElm.Domain.Entities;
+﻿using TalebElm.Domain.Entities;
 
-namespace TalebElm.Domain.Interfaces
+namespace TalebElm.Domain.Interfaces;
+
+public interface IUserRepository : IRepository<User>
 {
-    public interface IUserRepository:IRepository<User>
-    {
-
-    }
 }
