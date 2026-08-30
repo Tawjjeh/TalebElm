62e1055acf61411e8adae23726198a7ee741e0b0
Abdullah Hashem <abdallahhashem326@gmail.com>
2026-08-30 06:27:57 +0300

    IModuleReposotiry Created Sucessfully

---
+++ src/TalebElm.Domain/Interfaces/IModuleRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TalebElm.Domain.Entities;
+
+namespace TalebElm.Domain.Interfaces
+{
+    public interface IModuleRepository : IRepository<Module> { }
+}
