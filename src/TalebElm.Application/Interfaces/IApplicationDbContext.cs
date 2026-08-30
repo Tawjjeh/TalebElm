52737a82ce58aee0320f3655800bc40fbcab3bf5
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-08-30 21:44:43 +0300

    feat: add IApplicationDbContext interface

---
+++ src/TalebElm.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalebElm.Application.Interfaces
+{
+    public interface IApplicationDbContext { 
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default); 
+    }
+}
