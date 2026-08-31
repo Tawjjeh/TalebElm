ac46fc55acf81b150c13d1c0348105faa8d6b3c8
Abdullah Hashem <abdallahhashem326@gmail.com>
2026-08-31 06:54:29 +0300

    Exam Entity Added Sucessfully

---
+++ src/TalebElm.Domain/Entities/Exam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalebElm.Domain.Entities
+{
+    public class Exam : BaseEntity
+    {
+        public string Title { get; set; }= string.Empty;
+        public int PassThreshold { get; set; }
+        public Guid ModuleId { get; set; }
+    }
+}
