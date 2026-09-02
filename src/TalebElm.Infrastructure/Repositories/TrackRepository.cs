532985f8a6582c5e02ed2a05826471fbe8171f4d
mohamed5446 <ma701275@gmail.com>
2026-09-02 13:19:09 +0300

    feat : add TrackRepository class

---
+++ src/TalebElm.Infrastructure/Repositories/TrackRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TalebElm.Domain.Entities;
+using TalebElm.Domain.Interfaces;
+
+namespace TalebElm.Infrastructure.Repositories
+{
+    public class TrackRepository : ITrackRepository
+    {
+        public Task AddAsync(Track entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Track>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
