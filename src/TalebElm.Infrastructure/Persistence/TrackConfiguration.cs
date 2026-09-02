b3ff19d0e6f44c27f7e043b0b4dc8cdd2338b579
Abanoub <abanobyousry60@example.com>
2026-09-02 18:10:24 +0300

    feat(infrastructure): add TrackConfiguration class

---
+++ src/TalebElm.Infrastructure/Persistence/TrackConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TalebElm.Domain.Entities;
+
+namespace TalebElm.Infrastructure.Persistence;
+
+public class TrackConfiguration : IEntityTypeConfiguration<Track>
+{
+    public void Configure(EntityTypeBuilder<Track> builder)
+    {
+
+    }
+}
