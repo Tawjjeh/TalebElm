77cebdd7d2ef828fc287a69cf5c435e13a8bec94
Asma-Elhadiedy <asmaelhadiedy@gmail.com>
2026-09-22 23:12:08 +0300

    fix: remove commented code

--- src/TalebElm.Application/Services/IExamService.cs
+++ src/TalebElm.Application/Services/IExamService.cs
@@ -4,7 +4,7 @@ namespace TalebElm.Application.Services;
 
 public interface IExamService
 {
-    //Task<ExamResponse> CreateAsync(CreateExamRequest request);
-    //Task<ExamResponse> GetByIdAsync(Guid id);
-    //Task<ExamResultResponse> SubmitAsync(SubmitExamRequest request);
+    Task<ExamResponse> CreateAsync(CreateExamRequest request);
+    Task<ExamResponse> GetByIdAsync(Guid id);
+    Task<ExamResultResponse> SubmitAsync(SubmitExamRequest request);
 }   
