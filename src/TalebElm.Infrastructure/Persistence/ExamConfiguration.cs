b933745ee78bdc562a1636dd6ad33557e43e6f21
Asma-Elhadiedy <asmaelhadiedy@gmail.com>
2026-09-22 23:14:52 +0300

    fix: remove commented code

--- src/TalebElm.Infrastructure/Persistence/ExamConfiguration.cs
+++ src/TalebElm.Infrastructure/Persistence/ExamConfiguration.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TalebElm.Domain.Entities;
 
 namespace TalebElm.Infrastructure.Persistence;
 
-//public class ExamConfiguration : IEntityTypeConfiguration<Exam>
-//{
-//    public void Configure(EntityTypeBuilder<Exam> builder)
-//    {
-//    }
-//}
+public class ExamConfiguration : IEntityTypeConfiguration<Exam>
+{
+    public void Configure(EntityTypeBuilder<Exam> builder)
+    {
+    }
+}
